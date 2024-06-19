e8cff126f06a3eb1ebd87c78da6881b64cc6e5f7
exertieb <bastien.exertier@grenoble-inp.org>
2024-06-19 23:23:56 +0200

    add flag to enable/disable retrying once on 401

--- httpx_keycloak/_transport.py
+++ httpx_keycloak/_transport.py
@@ -6,9 +6,10 @@ from ._interfaces import AccessTokenProvider
 
 class AuthenticationTransportWrapper(httpx.BaseTransport):
 
-	def __init__(self, transport: httpx.BaseTransport, token_provider: AccessTokenProvider):
+	def __init__(self, transport: httpx.BaseTransport, token_provider: AccessTokenProvider, retry_on_401: bool=True):
 		self.transport = transport
 		self.token_provider = token_provider
+		self.retry_on_401 = retry_on_401
 
 	def handle_request(self, request: httpx.Request) -> httpx.Response:
 
@@ -18,7 +19,9 @@ class AuthenticationTransportWrapper(httpx.BaseTransport):
 
 			response = self.transport.handle_request(request)
 
-			if response.status_code == 401:
+			if response.status_code == 401 and self.retry_on_401:
 				request.headers['Authorization'] = self.token_provider.get_new_access_token().to_bearer_string()
+			else:
+				return response
 
 		return self.transport.handle_request(request)
