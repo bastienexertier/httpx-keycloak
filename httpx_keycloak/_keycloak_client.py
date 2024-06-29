812c63776a03e4fb978552fcbb4612cb98afac98
exertieb <bastien.exertier@grenoble-inp.org>
2024-06-30 00:41:36 +0200

    add auth method mapping

--- httpx_keycloak/_keycloak_client.py
+++ httpx_keycloak/_keycloak_client.py
@@ -1,6 +1,6 @@
 
 import datetime
-from typing import TypedDict, Optional
+from typing import TypedDict, Optional, Callable
 
 import httpx
 
@@ -27,20 +27,11 @@ class KeycloakClient:
 		self.now = datetime_provider or datetime.datetime.now
 		self.__openid_config: Optional[OpenIDConfiguration] = None
 
-
-	def load_openid_config(self) -> OpenIDConfiguration:
-		response = self.http.get('/.well-known/openid-configuration/')
-
-		if response.status_code == 404:
-			raise KeycloakError(f'OpenID configuration not found at {response.url}')
-
-		return response.json()
-
-	@property
-	def openid_config(self) -> OpenIDConfiguration:
-		if not self.__openid_config:
-			self.__openid_config = self.load_openid_config()
-		return self.__openid_config
+		self.__builders: dict[str, Callable[[TokenRequest, dict[str, str]], tuple[Optional[tuple[str, str]], dict[str, str]]]] = {
+			'client_secret_basic': self.__build_client_secret_basic,
+			'client_secret_post': self.__build_client_secret_post,
+			'client_secret_jwt': self.__build_client_secret_jwt,
+		}
 
 	def supports_grant(self, grant: GrantType) -> bool:
 		return grant in self.openid_config['grant_types_supported']
@@ -55,42 +46,28 @@ class KeycloakClient:
 
 		auth_methods_supported = openid_config['token_endpoint_auth_methods_supported']
 
-		auth = None
+		if isinstance(token_request.auth_methods, tuple):
+			auth_methods = token_request.auth_methods
+		elif isinstance(token_request.auth_methods, str):
+			auth_methods = (token_request.auth_methods,)
+		else:
+			auth_methods = ('client_secret_basic', 'client_secret_post')
+
+		auth_method = next((
+			self.__builders[method]
+			for method in auth_methods
+			if method in auth_methods_supported
+			and method in self.__builders
+		), None)
+
+		if auth_method is None:
+			raise KeycloakError(f'None of the requested auth method is supported: {str.join(", ", auth_methods)}')
+
 		request_body: dict[str, str]
 		request_body = {'grant_type': token_request.grant_type}
 		request_body |= token_request.to_request_body()
 
-		auth_method = None
-		for auth_method in token_request.auth_methods:
-
-			if auth_method not in auth_methods_supported:
-				continue
-
-		if auth_method == 'client_secret_basic':
-			auth = httpx.BasicAuth(token_request.client_id, token_request.client_secret or '')
-		elif auth_method == 'client_secret_post':
-			request_body['client_id'] = token_request.client_id
-			if token_request.client_secret:
-				request_body['client_secret'] = token_request.client_secret
-		elif auth_method == 'client_secret_jwt':
-			import time
-			import uuid
-			import jwt
-			epoch_time = int(time.time())
-			client_assertion = jwt.encode({
-				"iss": token_request.client_id,
-				"sub": token_request.client_id,
-				"aud": self.openid_config['token_endpoint'],
-				"jti": str(uuid.uuid4()),
-				"exp": epoch_time+1000
-			}, token_request.client_secret, algorithm="HS256")
-
-			request_body['client_assertion_type'] = 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
-			request_body['client_assertion'] = client_assertion
-
-
-		else:
-			raise KeycloakError('No token auth method supported')
+		auth, request_body = auth_method(token_request, request_body)
 
 		if token_request.scopes:
 			request_body["scope"] = str.join(" ", token_request.scopes)
@@ -107,3 +84,44 @@ class KeycloakClient:
 			raise KeycloakError(f"[{response.status_code}] {data['error']} - {data['error_description']}")
 
 		return KeycloakToken.from_dict(data, emitted_at=self.now() - response.elapsed)
+
+
+	def load_openid_config(self) -> OpenIDConfiguration:
+		response = self.http.get('/.well-known/openid-configuration/')
+
+		if response.status_code == 404:
+			raise KeycloakError(f'OpenID configuration not found at {response.url}')
+
+		return response.json()
+
+	@property
+	def openid_config(self) -> OpenIDConfiguration:
+		if not self.__openid_config:
+			self.__openid_config = self.load_openid_config()
+		return self.__openid_config
+
+
+
+	def __build_client_secret_basic(self, request: TokenRequest, data: dict[str, str]) -> tuple[Optional[tuple[str, str]], dict[str, str]]:
+		return (request.client_id, request.client_secret or ''), data
+
+	def __build_client_secret_post(self, request: TokenRequest, data: dict[str, str]) -> tuple[Optional[tuple[str, str]], dict[str, str]]:
+		data['client_id'] = request.client_id
+		if request.client_secret:
+			data['client_secret'] = request.client_secret
+		return None, data
+
+	def __build_client_secret_jwt(self, request: TokenRequest, data: dict[str, str]) -> tuple[Optional[tuple[str, str]], dict[str, str]]:
+		import uuid
+		import jwt
+		client_assertion = jwt.encode({
+			"iss": request.client_id,
+			"sub": request.client_id,
+			"aud": self.openid_config['token_endpoint'],
+			"jti": str(uuid.uuid4()),
+			"exp": self.now().timestamp()+1000
+		}, request.client_secret, algorithm="HS256")
+
+		data['client_assertion_type'] = 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer'
+		data['client_assertion'] = client_assertion
+		return None, data
