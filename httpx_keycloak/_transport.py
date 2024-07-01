a3f84bb17156891afd621b5a42860e03ef5089cd
exertieb <bastien.exertier@grenoble-inp.org>
2024-07-01 23:59:32 +0200

    again

--- httpx_keycloak/_transport.py
+++ httpx_keycloak/_transport.py
@@ -1,7 +1,6 @@
 
 import datetime
-from typing import Optional, cast
-from dataclasses import dataclass
+from typing import Callable, Optional
 
 import httpx
 
@@ -12,35 +11,21 @@ from ._token import KeycloakToken
 from ._token_provider import TokenProvider
 
 
-@dataclass(frozen=True)
-class AuthenticationTransportSettings:
-
-	retry_on_401: bool = True
-	override_existing_auth_header: bool = True
-
-
 class AuthenticatingTransport(httpx.BaseTransport):
 
 	def __init__(
 		self,
 		transport: httpx.BaseTransport,
-		credentials: Credentials,
+		credentials_builder: Callable[[httpx.Request], Optional[Credentials]],
 		token_provider: TokenProvider,
-		settings: Optional[AuthenticationTransportSettings]=None
 	):
 		self.transport = transport
-		self.credentials = credentials
+		self.credentials_builder = credentials_builder
 		self.token_provider = token_provider
-		self.settings = settings or AuthenticationTransportSettings()
-
-	def build_credentials(self, request: httpx.Request) -> Optional[Credentials]:
-		if 'Authorization' in request.headers and not self.settings.override_existing_auth_header:
-			return None
-		return self.credentials
 
 	def handle_request(self, request: httpx.Request) -> httpx.Response:
 
-		credentials = self.build_credentials(request)
+		credentials = self.credentials_builder(request)
 
 		response = None
 		if credentials:
@@ -51,27 +36,10 @@ class AuthenticatingTransport(httpx.BaseTransport):
 
 				response = self.transport.handle_request(request)
 
-				if response.status_code != 401 or not self.settings.retry_on_401:
+				if response.status_code != 401:
 					return response
 
 		return response or self.transport.handle_request(request)
-
-
-class TokenExchangeAuthenticationTransport(AuthenticatingTransport):
-
-	def build_credentials(self, request: httpx.Request) -> Optional[Credentials]:
-
-		auth_header: str = request.headers.get('Authorization')
-
-		if not auth_header:
-			raise KeycloakError('Token to be exchanged not found in Authorization header')
-
-		return cast(SupportsExhange, self.credentials).exchange(auth_header.removeprefix('Bearer '))
-
-
-def set_auth_header(request: httpx.Request, token: KeycloakToken):
-	request.headers['Authorization'] = token.to_bearer_string()
-
 
 
 class AuthenticatingTransportFactory:
@@ -81,19 +49,37 @@ class AuthenticatingTransportFactory:
 		self.now = datetime_provider or datetime.datetime.now
 
 	def client_credentials_transport(self, transport: httpx.BaseTransport, credentials: ClientCredentials) -> httpx.BaseTransport:
-		return AuthenticatingTransport(transport, credentials, TokenProvider(
-			self.keycloak,
-			self.now,
-		))
+		return AuthenticatingTransport(
+			transport,
+			lambda req: build_credentials(req, credentials),
+			TokenProvider(self.keycloak, self.now)
+		)
 
 	def resource_owner_transport(self, transport: httpx.BaseTransport, credentials: ResourceOwnerCredentials) -> httpx.BaseTransport:
-		return AuthenticatingTransport(transport, credentials, TokenProvider(
-			self.keycloak,
-			self.now,
-		))
+		return AuthenticatingTransport(
+			transport,
+			lambda req: build_credentials(req, credentials),
+			TokenProvider(self.keycloak, self.now)
+		)
 
 	def token_exchange_transport(self, transport: httpx.BaseTransport, credentials: SupportsExhange) -> httpx.BaseTransport:
-		return TokenExchangeAuthenticationTransport(transport, credentials, TokenProvider(
-			self.keycloak,
-			self.now,
-		))
+		return AuthenticatingTransport(
+			transport,
+			lambda req: build_exchange_credentials(req, credentials),
+			TokenProvider(self.keycloak, self.now)
+		)
+
+
+def set_auth_header(request: httpx.Request, token: KeycloakToken):
+	request.headers['Authorization'] = token.to_bearer_string()
+
+def build_credentials(request: httpx.Request, credentials: Credentials) -> Optional[Credentials]:
+	return credentials if 'Authorization' not in request.headers else None
+
+def build_exchange_credentials(request: httpx.Request, credentials: SupportsExhange) -> Optional[Credentials]:
+	auth_header: str = request.headers.get('Authorization')
+
+	if not auth_header:
+		raise KeycloakError('Token to be exchanged not found in Authorization header')
+
+	return credentials.exchange(auth_header.removeprefix('Bearer '))
