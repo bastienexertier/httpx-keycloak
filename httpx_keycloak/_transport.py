bacd645ebc0559cc2452cd06d03f33dc3874d6e1
exertieb <bastien.exertier@grenoble-inp.org>
2024-07-01 23:40:34 +0200

    refactor transport

--- httpx_keycloak/_transport.py
+++ httpx_keycloak/_transport.py
@@ -1,12 +1,11 @@
 
 import datetime
-from typing import Optional, Union
+from typing import Optional, cast
 from dataclasses import dataclass
 
 import httpx
 
-
-from ._interfaces import SupportsExhange, KeycloakError, DatetimeProvider
+from ._interfaces import Credentials, SupportsExhange, KeycloakError, DatetimeProvider
 from ._keycloak_client import KeycloakClient
 from ._model import ClientCredentials, ResourceOwnerCredentials
 from ._token import KeycloakToken
@@ -20,12 +19,12 @@ class AuthenticationTransportSettings:
 	override_existing_auth_header: bool = True
 
 
-class ClientAuthenticationTransport(httpx.BaseTransport):
+class AuthenticatingTransport(httpx.BaseTransport):
 
 	def __init__(
 		self,
 		transport: httpx.BaseTransport,
-		credentials: Union[ClientCredentials, ResourceOwnerCredentials],
+		credentials: Credentials,
 		token_provider: TokenProvider,
 		settings: Optional[AuthenticationTransportSettings]=None
 	):
@@ -34,64 +33,40 @@ class ClientAuthenticationTransport(httpx.BaseTransport):
 		self.token_provider = token_provider
 		self.settings = settings or AuthenticationTransportSettings()
 
+	def build_credentials(self, request: httpx.Request) -> Optional[Credentials]:
+		if 'Authorization' in request.headers and not self.settings.override_existing_auth_header:
+			return None
+		return self.credentials
+
 	def handle_request(self, request: httpx.Request) -> httpx.Response:
 
-		if 'Authorization' in request.headers and not self.settings.override_existing_auth_header:
-			return self.transport.handle_request(request)
+		credentials = self.build_credentials(request)
 
 		response = None
-		for token in self.token_provider.get_token(self.credentials):
+		if credentials:
 
-			set_auth_header(request, token)
+			for token in self.token_provider.get_token(credentials):
 
-			response = self.transport.handle_request(request)
+				set_auth_header(request, token)
 
-			if response.status_code != 401 or not self.settings.retry_on_401:
-				return response
+				response = self.transport.handle_request(request)
 
-		if response:
-			return response
+				if response.status_code != 401 or not self.settings.retry_on_401:
+					return response
 
-		return self.transport.handle_request(request)
+		return response or self.transport.handle_request(request)
 
 
-class TokenExchangeAuthenticationTransport(httpx.BaseTransport):
+class TokenExchangeAuthenticationTransport(AuthenticatingTransport):
 
-	def __init__(
-		self,
-		transport: httpx.BaseTransport,
-		credentials: SupportsExhange,
-		token_provider: TokenProvider,
-		settings: Optional[AuthenticationTransportSettings]=None
-	):
-		self.transport = transport
-		self.credentials = credentials
-		self.token_provider = token_provider
-		self.settings = settings or AuthenticationTransportSettings()
-
-	def handle_request(self, request: httpx.Request) -> httpx.Response:
+	def build_credentials(self, request: httpx.Request) -> Optional[Credentials]:
 
 		auth_header: str = request.headers.get('Authorization')
 
 		if not auth_header:
-			raise KeycloakError('Missing Authorization header')
+			raise KeycloakError('Token to be exchanged not found in Authorization header')
 
-		subject_token = auth_header.removeprefix('Bearer ')
-
-		response = None
-		for token in self.token_provider.get_token(self.credentials.exchange(subject_token)):
-
-			set_auth_header(request, token)
-
-			response = self.transport.handle_request(request)
-
-			if response.status_code != 401 or not self.settings.retry_on_401:
-				return response
-
-		if response:
-			return response
-
-		return self.transport.handle_request(request)
+		return cast(SupportsExhange, self.credentials).exchange(auth_header.removeprefix('Bearer '))
 
 
 def set_auth_header(request: httpx.Request, token: KeycloakToken):
@@ -106,13 +81,13 @@ class AuthenticatingTransportFactory:
 		self.now = datetime_provider or datetime.datetime.now
 
 	def client_credentials_transport(self, transport: httpx.BaseTransport, credentials: ClientCredentials) -> httpx.BaseTransport:
-		return ClientAuthenticationTransport(transport, credentials, TokenProvider(
+		return AuthenticatingTransport(transport, credentials, TokenProvider(
 			self.keycloak,
 			self.now,
 		))
 
 	def resource_owner_transport(self, transport: httpx.BaseTransport, credentials: ResourceOwnerCredentials) -> httpx.BaseTransport:
-		return ClientAuthenticationTransport(transport, credentials, TokenProvider(
+		return AuthenticatingTransport(transport, credentials, TokenProvider(
 			self.keycloak,
 			self.now,
 		))
